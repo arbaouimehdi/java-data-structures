794877b56b3020ebd6ea7d0a6d6d54f7b3ab08a8
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-11-30 06:33:05 +0000

    Create the Stack Class

---
+++ BasicStack.java
@@ -0,0 +1,11 @@
+public class BasicStack<X> {
+  private X [] data;
+
+  public BasicStack(){
+    // Initalize a data structure
+    // to a new object array of 1000
+    // it means hlding 1000 items in our stack
+    data = (X[]) new Object[1000];
+  }
+
+}
